83cdfa4e41dd7ccb20e40084d95e78cb3004f4b9
clo-cool <clodaghhawkes@gmail.com>
2024-11-04 15:59:39 +0000

    Add files via upload

---
+++ dictionary/dictionary.py
@@ -0,0 +1,15 @@
+# creating a dictionary
+
+fruit = ["apple","sdfgh","banana"]
+age = [59,6,37,1234567890]
+
+#creating address dictionary
+
+address = {
+    "name":"aisling",
+    "home":"castlegragry",
+    "socity":"xyz",
+    "city":"new york",
+    "country":"USA"
+}
+print(address)
